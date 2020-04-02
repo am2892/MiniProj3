19aa0622ffbaf9af8509121a953ed02a993a8473
Sherif Elmetwally <sge5@njit.edu>
2020-04-02 19:05:22 -0400

    add table for delete

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 - [x] let user enter a list of numbers and get mean,median,mode,variance
 - [x] let user signup/login
 - [x] let user keep track of previous functions 
-- [ ] add styles
-- [ ] convert dropdown to buttons
+- [x] add styles
+- [x] convert dropdown to buttons
+- [x] let users view their history
+- [x] let users delete all of their history
+- [x] let users delete a specific function from their history
+- [ ] let users bring back a specific function from their history
--- project/models.py
+++ project/models.py
@@ -1,4 +1,6 @@
 from flask_login import UserMixin
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
 from . import db
 
 class User(UserMixin, db.Model):
@@ -6,3 +8,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class functions(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    userName = db.Column(db.String(1000))
+    functionName = db.Column(db.String(1000))
+    numbers = db.Column(db.String(1000))
